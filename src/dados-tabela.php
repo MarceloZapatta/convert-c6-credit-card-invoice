afce1d659bd5b71ecb979bde53d914da4c0bc93d
Marcelo Zapatta <marcelozapatta0@gmail.com>
2024-09-21 21:24:34 -0300

    feat: exporting to xlsx

--- src/dados-tabela.php
+++ src/dados-tabela.php
@@ -2,54 +2,91 @@
 
 require __DIR__ .  DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $lines = file(__DIR__ . DIRECTORY_SEPARATOR . 'dados-tabela.txt');
 
 $table = [];
 $row = [];
 
+/** Used to skip next line because money is always duplicated */
+$skipLines = 0;
+
 foreach ($lines as $line) {
+    if ($skipLines > 0) {
+        $skipLines--;
+        continue;
+    }
+
+    if (isRowFilled($row)) {
+        var_dump('fillRow', $row);
+        $table[] = $row;
+        $row = [
+            'date' => $row['date']
+        ];
+    }
+
     $lineSanitize = trim($line);
 
     if ($lineSanitize === '') {
+        var_dump('linha em branco');
+        continue;
+    }
+
+    if (stringContainsPaymentIncludeString($lineSanitize)) {
+        $skipLines = 3;
+        continue;
+    }
+
+    if (stringContainsExternalCurrencyValue($lineSanitize)) {
+        continue;
+    }
+    
+    if (stringContainsInstallments($lineSanitize)) {
+        var_dump('externalCurrency, installments', $lineSanitize);
         continue;
     }
 
     $date = stringContainsDate($lineSanitize);
 
     if ($date) {
-        if (!empty($row)) {
-            $table[] = $row;
-            $row = [];
-        }
-
+        var_dump('date', $date);
         $row['date'] = $date;
         continue;
     }
 
     if (stringContainsMoney($lineSanitize)) {
+        var_dump('money', $lineSanitize);
         $value = getValue($lineSanitize);
         $row['value'] = $value;
+        $skipLines = 1;
         continue;
     }
 
     if (stringContainsCategory($lineSanitize)) {
+        var_dump('category', $lineSanitize);
         $row['categories'] = $lineSanitize;
         continue;
     }
 
-    if (stringContainsExternalCurrencyValue($lineSanitize) || stringContainsInstallments($lineSanitize)) {
-        continue;
-    }
-
+    var_dump('passou tudo description', $lineSanitize);
+    
     $row['description'] = $lineSanitize;
 }
 
-$table[] = $row;
+var_dump($row, 'oq esta vazio??');
+if (isRowFilled($row)) {
+    var_dump('fillRow', $row);
+    $table[] = $row;
+    $row = [];
+}
 
 $data = generateCSVData($table);
 
 file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'dados-tabela.csv', $data);
+
+generateXlsxFile($table);
 
 echo 'File exported succesfully!!';
 
@@ -66,6 +103,17 @@ function stringContainsDate(string $string): ?string
     preg_match($pattern, $string, $matches);
 
     return $matches[0] ?? null;
+}
+
+/**
+ * Check if the stirng is "Inclusao de pagemento"
+ *
+ * @param string $string
+ * @return boolean
+ */
+function stringContainsPaymentIncludeString(string $string): bool
+{
+    return $string === 'Inclusao de Pagamento';  
 }
 
 /**
@@ -98,6 +146,7 @@ function stringContainsMoney(string $string): bool
  */
 function stringContainsCategory(string $string): bool
 {
+    $stringCategories = explode(' / ', $string);
     $categories = [
         "Supermercados",
         "Mercearia",
@@ -130,11 +179,13 @@ function stringContainsCategory(string $string): bool
         "Entretenimento",
         "Vestuário",
         "Roupas",
-        "Especialidade varejo"
+        "Especialidade varejo",
+        "IOF Transacoes Exterior R$",
+        "T&E"
     ];
 
     foreach ($categories as $category) {
-        if (strpos($string, $category) !== false) {
+        if (in_array($category, $stringCategories)) {
             return true;
         }
     }
@@ -164,10 +215,13 @@ function stringContainsInstallments(string $string): bool
  */
 function getValue(string $string): float
 {
-    $money = substr($string, 2);
+    $signal = (substr($string, 0, 2) === 'R$') ? 1 : -1;
+
+    $money = $signal === 1 ? substr($string, 2) : substr($string, 3);
+    
     $money = str_replace('.', '', $money);
     $money = str_replace(',', '.', $money);
-    return (float) $money;
+    return (float) $money * $signal;
 }
 
 /**
@@ -185,4 +239,48 @@ function generateCSVData(array $table): string
     }
 
     return $csvData;
+}
+
+/**
+ * Generate XLSX file
+ *
+ * @param array $table
+ * @return void
+ */
+function generateXlsxFile(array $table): void {
+    $spreadsheet = new Spreadsheet();
+    $activeWorksheet = $spreadsheet->getActiveSheet();
+    
+    $activeWorksheet->setCellValue('A1', 'Data')
+        ->setCellValue('B1', 'Descrição')
+        ->setCellValue('C1', 'Valor')
+        ->setCellValue('D1', 'Realizado')
+        ->setCellValue('E1', 'Categoria');
+
+    $i = 1;
+    foreach ($table as $row) {
+        $i++;
+
+        $activeWorksheet->setCellValue("A{$i}", $row['date']);
+        $activeWorksheet->setCellValue("B{$i}", $row['description']);
+        $activeWorksheet->setCellValue("C{$i}", $row['value']);
+        $activeWorksheet->setCellValue("D{$i}", 'Não');
+        $activeWorksheet->setCellValue("E{$i}", $row['categories']);
+    }
+
+    $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
+    $writer->save(__DIR__ . DIRECTORY_SEPARATOR . 'dados-tabela.xlsx');
+
+    $spreadsheet->disconnectWorksheets();
+    unset($spreadsheet);
+}
+
+/**
+ * Check if all fields in the row are filled
+ *
+ * @param array $row
+ * @return boolean
+ */
+function isRowFilled(array $row): bool {
+    return !empty($row['date']) && !empty($row['description']) && !empty($row['value']) && !empty($row['categories']);
 }
